b34fd5375c0c17ef11970cf9971b9eec097d0363
Usama Bin Imran <40295855+Usamabinimran97@users.noreply.github.com>
2023-06-14 12:48:25 +0500

    added managers

---
+++ Assets/_DogGame/Scripts/Managers/AdsManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class AdsManager : MonoBehaviour
+{
+    public static AdsManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+}
---
+++ Assets/_DogGame/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+   public static LevelManager Instance;
+   
+       private void Awake()
+       {
+           if (Instance == null)
+               Instance = this;
+       }
+}
